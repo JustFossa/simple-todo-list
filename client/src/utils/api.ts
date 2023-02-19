bf41828366d6d556be65286dcee7c23ac8cbcec9
JustFossa <panko.marek@icloud.com>
2023-02-19 16:13:39 +0100

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
